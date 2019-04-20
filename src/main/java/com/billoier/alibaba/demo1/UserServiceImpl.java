ac068cdc98dfb17a107e0b86ed33f99d924d923d
yuetao <yuetao.wyt@alibaba-inc.com>
2019-04-20 22:24:45 +0800

    tets new Branch

--- src/main/java/com/billoier/alibaba/demo1/UserServiceImpl.java
+++ src/main/java/com/billoier/alibaba/demo1/UserServiceImpl.java
@@ -5,5 +5,6 @@ public class UserServiceImpl implements UserService {
     @Override
     public void sayHello() {
         System.out.println("Hello Spring");
+        System.out.println("Blue is the color");
     }
 }
